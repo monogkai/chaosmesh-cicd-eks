a0fa7280c532206dddfcadd726d42aa31831a656
David Martins <davidpmartins1995@hotmail.com>
2025-09-16 20:49:48 +0100

    Add terraform backend

---
+++ terraform/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket = "placeholder"
+    key    = "placeholder"
+    region = "placeholder"
+    encrypt = true
+  }
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,3 +7,13 @@ variable "aws_account_id" {
   description = "AWS Account ID"
   type        = string
 }
+
+variable "aws_tf_bucket" {
+  description = "Terraform S3 Bucket Name"
+  type        = string
+}
+
+variable "aws_tf_key" {
+  description = "Terraform S3 Bucket Key"
+  type        = string
+}
