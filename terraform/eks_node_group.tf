2e8f5ed3389e011f3bdea1742308769750f3570a
David Martins <davidpmartins1995@hotmail.com>
2025-09-19 16:02:20 +0100

    Add new tests

--- terraform/eks_node_group.tf
+++ terraform/eks_node_group.tf
@@ -6,9 +6,11 @@ resource "aws_eks_node_group" "default" {
 
   scaling_config {
     desired_size = 2
-    max_size     = 3
+    max_size     = 2
     min_size     = 1
   }
+
+  instance_types = ["t3.small"]
 
   depends_on = [
     aws_eks_cluster.ci_cd_cluster,
